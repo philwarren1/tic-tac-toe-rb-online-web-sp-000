cd3a2eaccd1067c9bd81d06f4dcb2a06ce112e74
Philip Warren <philwarren1@gmail.com>
2020-07-19 16:23:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,7 +90,7 @@ end
 def draw?(board)
    if !won?(board) && full?(board)
      return true
-   elsif !won?(board) && !full?(board)
+   elsif !won?(board)
      return false
    else won?(board)
      return false
